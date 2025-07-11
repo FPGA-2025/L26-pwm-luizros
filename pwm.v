d866c9775314b12ed33a442ec867057e34185aaa
Luiz Felipe <64107306+luizros@users.noreply.github.com>
2025-07-10 21:53:20 -0300

    Update pwm.v

--- pwm.v
+++ pwm.v
@@ -1,9 +1,28 @@
 module PWM (
-    input wire clk,
-    input wire rst_n,
-    input wire [15:0] duty_cycle, // duty_cycle = period * duty_porcent, 0 <= duty_porcent <= 1
-    input wire [15:0] period, // clk_freq / pwm_freq = period
+    input  wire clk,
+    input  wire rst_n,
+    input  wire [15:0] duty_cycle,
+    input  wire [15:0] period,
     output reg pwm_out
 );
+
+    reg [15:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter  <= 0;
+            pwm_out <= 0;
+        end else begin
+            if (counter >= period - 1)
+                counter <= 0;
+            else
+                counter <= counter + 1;
+
+            if (counter < duty_cycle)
+                pwm_out <= 1;
+            else
+                pwm_out <= 0;
+        end
+    end
 
 endmodule
