bd5884e12bac73131f3beb3aa8917e7e42bbf829
Luiz Felipe <64107306+luizros@users.noreply.github.com>
2025-07-10 21:54:05 -0300

    teste.v

--- pwm_control.v
+++ pwm_control.v
@@ -6,12 +6,50 @@ module PWM_Control #(
     input  wire rst_n,
     output wire [7:0] leds
 );
-    localparam integer PWM_CLK_PERIOD = CLK_FREQ / PWM_FREQ;
-    localparam integer PWM_DUTY_CYCLE = 50; // 0.0025% duty cycle
 
-    localparam SECOND         = CLK_FREQ;
-    localparam HALF_SECOND    = SECOND / 2;
-    localparam QUARTER_SECOND = SECOND / 4;
-    localparam EIGHTH_SECOND  = SECOND / 8;
+    localparam integer PWM_CLK_PERIOD = CLK_FREQ / PWM_FREQ;
+    localparam [15:0] DUTY_MIN = PWM_CLK_PERIOD / 40000;  // ~0.0025%
+    localparam [15:0] DUTY_MAX = (PWM_CLK_PERIOD * 70) / 100; // 70%
+
+    reg [15:0] duty_cycle;
+    reg [15:0] delay_counter;
+    reg        up;
+
+    wire pwm_out;
+
+    PWM pwm_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .duty_cycle(duty_cycle),
+        .period(PWM_CLK_PERIOD),
+        .pwm_out(pwm_out)
+    );
+
+    assign leds = {8{pwm_out}};
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            duty_cycle    <= DUTY_MIN;
+            delay_counter <= 0;
+            up            <= 1;
+        end else begin
+            delay_counter <= delay_counter + 1;
+            if (delay_counter >= CLK_FREQ / 200) begin  // ajusta fade: ~5ms
+                delay_counter <= 0;
+
+                if (up) begin
+                    if (duty_cycle < DUTY_MAX)
+                        duty_cycle <= duty_cycle + 1;
+                    else
+                        up <= 0;
+                end else begin
+                    if (duty_cycle > DUTY_MIN)
+                        duty_cycle <= duty_cycle - 1;
+                    else
+                        up <= 1;
+                end
+            end
+        end
+    end
 
 endmodule
